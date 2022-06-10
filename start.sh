1e5346c7ee7bd7202c4e7833639c4f0ee72b061b
maxd <raydoom.163.com>
2022-06-10 15:32:34 +0800

    .

--- start.sh
+++ start.sh
@@ -5,8 +5,9 @@ mkdir /etc/nginx/http.d/certs
 cp -r /opt/sh/localhost.localdomain/* /etc/nginx/http.d/certs
 
 # start nginx
+sed -i "s/port/${PORT}/g" /etc/nginx/http.d/default.conf
 nginx
-/bin/shaenvsubst '\$UUID,\$WS_PATH' < $config_path > /usr/local/etc/v2ray/config.json
+
 # start v2ray
 sed -i "s/uuid/${UUID}/g" /etc/v2ray/config.json
 nohup v2ray --config /etc/v2ray/config.json > /var/log/v2ray.log 2>&1 &
